f2dbe2a5be2a524daf19afb360c20c885f33e1b8
Nikita Danilov <danilovn1@mail.ru>
2025-09-13 14:47:02 +0500

    hm3_v1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, contacts_page
+from .views import home_page, contacts_page, product_detail
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('contacts/', contacts_page, name='contacts'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home_page(request):
-    # Эта функция будет рендерить (отображать) главную страницу
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()  # Получаем все продукты из БД
+    context = {
+        'products': products
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts_page(request):
     # Эта функция будет рендерить страницу контактов
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    # Используем get_object_or_404, чтобы получить товар или вернуть ошибку 404, если он не найден
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/product_detail.html', context)
